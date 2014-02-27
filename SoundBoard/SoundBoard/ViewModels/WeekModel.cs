4dd5a038b64c7e060d97e429315a7b7f22495419
talk2mayor30 <talk2mayor30@gmail.com>
2014-02-27 19:42:30 +0100

    next commit

---
+++ SoundBoard/SoundBoard/ViewModels/WeekModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoundBoard.ViewModels
+{
+    public class WeekModel
+    {
+        public WeekModel()
+        {
+
+        }
+        public WeekModel(string id, string week, string content)
+        {
+            this.ID = id;
+            this.Week = week;
+            this.WeekContent = content;
+     
+
+        }
+        public string Week { get; set; }
+        public string WeekContent { get; set; }
+        public string WeekPython { get; set; }
+        public string ID { get; set; }
+
+         
+    }
+}
